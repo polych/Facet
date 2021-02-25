9a7c801087ccbea897d4ab56ed4ff7105bd2d966
Maksdiakun <43043815+Maksdiakun@users.noreply.github.com>
2021-02-25 13:14:46 +0200

    Updates

---
+++ precache-manifest.9cf0b680600c7e5ce68029d4b75221a3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29624704af4c04d23eca773d49fee249",
+    "url": "/Facet/index.html"
+  },
+  {
+    "revision": "d61c832dd63031a781d0",
+    "url": "/Facet/static/css/main.367d70ed.chunk.css"
+  },
+  {
+    "revision": "92592b3eb11e4642f6cf",
+    "url": "/Facet/static/js/2.f3436a6e.chunk.js"
+  },
+  {
+    "revision": "1cb357dccf04832ad24982ee1acc681b",
+    "url": "/Facet/static/js/2.f3436a6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d61c832dd63031a781d0",
+    "url": "/Facet/static/js/main.49a48f7c.chunk.js"
+  },
+  {
+    "revision": "1d378b33ddccfa552026",
+    "url": "/Facet/static/js/runtime-main.de4de0f9.js"
+  },
+  {
+    "revision": "801965b0436842e843141a5a6c1e387f",
+    "url": "/Facet/static/media/Comfortaa-Bold.801965b0.ttf"
+  },
+  {
+    "revision": "1c593a568b347510402764c6b8e93a37",
+    "url": "/Facet/static/media/Comfortaa-Medium.1c593a56.ttf"
+  },
+  {
+    "revision": "c2488bfda16e6a811f62414da90e51e4",
+    "url": "/Facet/static/media/Comfortaa-Regular.c2488bfd.ttf"
+  },
+  {
+    "revision": "d905de658a9c1fe4f9d779ccb15c7433",
+    "url": "/Facet/static/media/Comfortaa-SemiBold.d905de65.ttf"
+  },
+  {
+    "revision": "93bf8ed0c2e7b293b8586717b5df063e",
+    "url": "/Facet/static/media/a.93bf8ed0.svg"
+  },
+  {
+    "revision": "581836db6ce83a74640ac8cffcb77f30",
+    "url": "/Facet/static/media/chart.581836db.svg"
+  },
+  {
+    "revision": "a00be1d81947071abcf4233cc192749d",
+    "url": "/Facet/static/media/chart2.a00be1d8.svg"
+  },
+  {
+    "revision": "4de810493cff577443d9f7bab2b77fd1",
+    "url": "/Facet/static/media/photo.4de81049.png"
+  },
+  {
+    "revision": "083106e9b8392b36c02d69313c85e873",
+    "url": "/Facet/static/media/table.083106e9.svg"
+  }
+]);
